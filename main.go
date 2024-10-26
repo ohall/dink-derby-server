eb2a2ef2f2f88e245250da2593e260c3f0b0d9e7
ohall <ohall@grassroots-tech.com>
2024-10-26 06:16:04 -0400

    mongo details

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
+func routes() *mux.Router {
 	router := mux.NewRouter()
-
 	// User routes
 	router.HandleFunc("/api/users", getUsers).Methods("GET")
 	router.HandleFunc("/api/users", createUser).Methods("POST")
@@ -29,6 +33,45 @@ func main() {
 	router.HandleFunc("/api/locations", getLocations).Methods("GET")
 	router.HandleFunc("/api/locations", createLocation).Methods("POST")
 
+	return router
+}
+
+func initializeMongoDB() (*mongo.Client, error) {
+	// Set client options from env variable for connection string
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI"))
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to MongoDB!")
+	return client, nil
+}
+
+func main() {
+	router := routes()
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	dbService, err := NewMongoDBService(os.Getenv("MONGO_CONNECTION_STRING"))
+	if err != nil {
+		log.Fatalf("Error initializing MongoDB service: %v", err)
+	}
+
 	log.Println("Server started on port 3000")
-	log.Fatal(http.ListenAndServe(":3000", router))
+	log.Println("DB Service: ", dbService)
+	log.Fatal(http.ListenAndServe("localhost:3000", router))
 }
