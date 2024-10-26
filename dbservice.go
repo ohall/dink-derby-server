af4e21528f9e73c340d77ed27a3d4d43e220fe37
ohall <ohall@grassroots-tech.com>
2024-10-26 06:15:44 -0400

    add mongo db service

---
+++ dbservice.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoDBService struct {
+	client *mongo.Client
+}
+
+func NewMongoDBService(uri string) (*MongoDBService, error) {
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return &MongoDBService{client: client}, nil
+}
+
+func (s *MongoDBService) InsertDocument(database, collection string, document interface{}) (*mongo.InsertOneResult, error) {
+	coll := s.client.Database(database).Collection(collection)
+	insertResult, err := coll.InsertOne(context.TODO(), document)
+	if err != nil {
+		return nil, err
+	}
+	return insertResult, nil
+}
+
+func (s *MongoDBService) FindDocument(database, collection string, filter interface{}) (*mongo.SingleResult, error) {
+	coll := s.client.Database(database).Collection(collection)
+	result := coll.FindOne(context.TODO(), filter)
+	return result, nil
+}
+
+func (s *MongoDBService) Close() error {
+	return s.client.Disconnect(context.TODO())
+}
